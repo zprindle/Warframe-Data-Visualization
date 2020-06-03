e81d45799b886f9f2a80e0fe0fee8708e783ca31
Zach Prindle <zacharypri@gmail.com>
2020-06-02 21:38:05 -0400

    Added dynamic button and data display

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
+        state: 0,
         platform: '',
         world_data: {}
     },
@@ -8,12 +9,17 @@ var app = new Vue({
         selectPlatform: function (event) {
             this.platform = event;
             this.getWorldState();
+            this.state += 1;
         },
         getWorldState: function() {
             let query = "https://api.warframestat.us/" + this.platform;
             axios
             .get(query)
             .then(response => (this.world_data = response))
+            .catch(error => console.log(error))
+        },
+        processResponse: function(response) {
+
         }
     }
   })
