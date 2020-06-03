6eafd0a766c9f67fe85a3754bb9031ff1a11c91d
Zach Prindle <zacharypri@gmail.com>
2020-06-02 21:09:02 -0400

    Added axios

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-        platform: ''
+        platform: '',
+        world_data: {}
     },
     methods: {
         selectPlatform: function (event) {
             this.platform = event;
+            this.getWorldState();
         },
         getWorldState: function() {
-
+            let query = "https://api.warframestat.us/" + this.platform;
+            axios
+            .get(query)
+            .then(response => (this.world_data = response))
         }
     }
   })
