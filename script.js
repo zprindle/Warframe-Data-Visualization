43ffa7acc6b23124e19cbeabc3ccab485c32dcdc
Zach Prindle <zacharypri@gmail.com>
2020-06-02 20:51:29 -0400

    Added platofrm buttons

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var app = new Vue({
     el: '#app',
     data: {
-        
+        platform: ''
+    },
+    methods: {
+        selectPlatform: function (event) {
+            this.platform = event;
+        },
+        getWorldState: function() {
+
+        }
     }
   })
